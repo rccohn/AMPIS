c46d87ca842978dfd1370346863f4d4e048956fe
Ryan Cohn <rcohn@andrew.cmu.edu>
2020-02-17 17:37:43 -0500

    updated requirements.txt, added source for pycocotools and detectron2, and added build_environment.sh to automate virtual environment creation

---
+++ build_environment.sh
@@ -0,0 +1,15 @@
+# create and activate virtual environment
+python3 -m venv ampis_venv
+source ampis_venv/bin/activate
+
+# install dependencies
+pip install -r requirements.txt
+
+# build pycocotools from source
+cd src/external/cocoapi/PythonAPI/
+make
+pip install -e .
+
+# build detectron2 from source
+cd ../../detectron2
+pip install -e .
