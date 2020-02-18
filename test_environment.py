f65872cf2eabc0adbac0f667b851ad9d29706d50
Ryan Cohn <rccohn@br006.pvt.bridges.psc.edu>
2020-02-17 19:22:57 -0500

    added environment test  to verify correcorrect installation of python and packages

--- test_environment.py
+++ test_environment.py
@@ -1,25 +1,33 @@
 import sys
 
-REQUIRED_PYTHON = "python3"
-
-
 def main():
     system_major = sys.version_info.major
-    if REQUIRED_PYTHON == "python":
-        required_major = 2
-    elif REQUIRED_PYTHON == "python3":
-        required_major = 3
-    else:
-        raise ValueError("Unrecognized python interpreter: {}".format(
-            REQUIRED_PYTHON))
+    system_minor = sys.version_info.minor
+    required_major = 3
+    required_minor = 6
+    print('checking python version major == 3 and minor version  >= 6')
+    print(sys.version_info)
+    if system_major != required_major or system_minor < required_minor:
+        raise('incorrect version. Please install python 3.6')
 
-    if system_major != required_major:
-        raise TypeError(
-            "This project requires Python {}. Found: Python {}".format(
-                required_major, sys.version))
-    else:
+    print('importing packages')
+    import torch
+    import torchvision
+    import pycocotools
+    import detectron2
+    from detectron2 import model_zoo
+    print('successfully imported torch, torchvision, pycocotools, detectron2, detectron2.model_zoo')
+    print('detecting gpu')
+    print('cuda gpu detected: {} gpu name: {}'.format(torch.cuda.is_available(), torch.cuda.get_device_name()))
+  
+   
+    
+    if torch.cuda.is_available():
         print(">>> Development environment passes all tests!")
-
+    else:
+        print('all tests pass but no gpu detected')
+    
+    
 
 if __name__ == '__main__':
     main()
