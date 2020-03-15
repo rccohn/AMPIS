f43f952ca53be9b9e31bf49c44b53bb4ab2220ee
Ryan Cohn <rcohn@cmu.edu>
2020-03-15 10:35:56 -0400

    resolved merge conflict?

--- notebooks/explore_data.py
+++ notebooks/explore_data.py
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-=======
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
->>>>>>> b4c32bc862a2b2b282635eb29ef6a4915a8a4921
 ##### Module imports
 
 gui = False
@@ -31,7 +22,6 @@ from detectron2 import model_zoo
 from detectron2.checkpoint import Checkpointer, PeriodicCheckpointer
 from detectron2.checkpoint import DetectionCheckpointer, PeriodicCheckpointer
 from detectron2.config import get_cfg
-<<<<<<< HEAD
 from detectron2.data import (
     DatasetCatalog,
     MetadataCatalog,
@@ -43,34 +33,15 @@ from detectron2.modeling import build_model
 from detectron2.solver import build_lr_scheduler, build_optimizer
 from detectron2.structures import BoxMode
 import detectron2.utils.comm as comm
-=======
-from detectron2.data import DatasetCatalog, MetadataCatalog
-from detectron2.engine import DefaultTrainer, DefaultPredictor
-from detectron2.structures import BoxMode
-from detectron2.utils.visualizer import Visualizer
->>>>>>> b4c32bc862a2b2b282635eb29ef6a4915a8a4921
 from detectron2.utils.events import (
     CommonMetricPrinter,
     EventStorage,
     JSONWriter,
     TensorboardXWriter,
 )
-<<<<<<< HEAD
 from detectron2.utils.visualizer import Visualizer
 
 import dataval # custom module for getting loss stats # TODO figure out how to get more meaningful loss statistics
-=======
-from detectron2.solver import build_lr_scheduler, build_optimizer
-from detectron2.data import (
-    MetadataCatalog,
-    build_detection_test_loader,
-    build_detection_train_loader,
-)
-
-from detectron2.modeling import build_model
-import detectron2.utils.comm as comm
-import dataval # custom module for getting loss stats
->>>>>>> b4c32bc862a2b2b282635eb29ef6a4915a8a4921
  
 # verify cuda is installed and running correctly
 import torch
@@ -78,12 +49,6 @@ from torch.utils.cpp_extension import CUDA_HOME
 print(torch.cuda.is_available(), CUDA_HOME)
 
 
-<<<<<<< HEAD
-=======
-# In[2]:
-
-
->>>>>>> b4c32bc862a2b2b282635eb29ef6a4915a8a4921
 ##### Load data in a way that is consistent with detectron2 formatting
 
 def get_data_dicts(json_path):
@@ -149,12 +114,9 @@ def get_data_dicts(json_path):
         dataset_dicts.append(record)
     return dataset_dicts
 
-<<<<<<< HEAD
 
 # In my data I have the training/validation data in a single VIA json file.
 # This function splits the data into distinct groups based on a specific attribute (specified by get_subset function)
-=======
->>>>>>> b4c32bc862a2b2b282635eb29ef6a4915a8a4921
 def split_data_dict(dataset_dicts, get_subset=None):
     """
     Splits data from json into subsets (ie training/validation/testing)
@@ -192,27 +154,16 @@ ddicts = get_data_dicts(json_path)
 
 subs = split_data_dict(ddicts)
 
-<<<<<<< HEAD
 
 # Store the data so that detectron2 can work with it
-=======
->>>>>>> b4c32bc862a2b2b282635eb29ef6a4915a8a4921
 for key, value in subs.items():
     DatasetCatalog.register("powder_" + key, lambda key=key: subs.get(key))
     MetadataCatalog.get("powder_" + key).set(thing_classes=["Powder"])
 
 
-<<<<<<< HEAD
 
 ##### Verify ground-truth masks are loaded correctly
 # overlays ground truth instances on images and saves them. 
-=======
-# In[5]:
-
-
-##### Verify ground-truth masks are loaded correctly
-
->>>>>>> b4c32bc862a2b2b282635eb29ef6a4915a8a4921
 for dataset in ['powder_Training', 'powder_Validation']:
     for d in DatasetCatalog.get(dataset):
         img_path = pathlib.Path(d['file_name'])
@@ -230,7 +181,6 @@ for dataset in ['powder_Training', 'powder_Validation']:
         plt.close('all')
 
 
-<<<<<<< HEAD
 
 ##### Set up detectron2 configurations for Mask R-CNN model
 
@@ -246,19 +196,6 @@ cfg.DATALOADER.NUM_WORKERS = 2
 
 weights_path = pathlib.Path('../','models','model_final_f10217.pkl') # path where downloaded weights is stored
 if weights_path.is_file():
-=======
-# In[10]:
-
-
-##### Set up configurations
-
-cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.DATASETS.TRAIN = ("powder_Training",)
-cfg.DATASETS.TEST = ("powder_Validation",)
-cfg.DATALOADER.NUM_WORKERS = 2
-if pathlib.Path('../','models','model_final_f10217.pkl').is_file():
->>>>>>> b4c32bc862a2b2b282635eb29ef6a4915a8a4921
     print('Weights found')
     cfg.MODEL.WEIGHTS = '../models/model_final_f10217.pkl'
 else:
@@ -267,49 +204,29 @@ else:
     cfg.MODEL.WEIGHTS = weights_source
     
 cfg.SOLVER.IMS_PER_BATCH = 1
-<<<<<<< HEAD
 cfg.SOLVER.BASE_LR = 0.00025  # Learning rate (default is 0.00025)
 cfg.SOLVER.CHECKPOINT_PERIOD = 1000 
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512   # default: 512
 
 #  Number of different classes of instances that will be predicted.
-=======
-cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-cfg.SOLVER.MAX_ITER = 20000   # 300 iterations seems good enough for this toy dataset; you may need to train longer for a practical dataset
-cfg.SOLVER.CHECKPOINT_PERIOD = 1000
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512   # faster, and good enough for this toy dataset (default: 512)
->>>>>>> b4c32bc862a2b2b282635eb29ef6a4915a8a4921
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (powder particle)
 
 # Maximum number of detections to return per image during inference (100 is
 # based on the limit established for the COCO dataset).
-<<<<<<< HEAD
 cfg.TEST.DETECTIONS_PER_IMAGE = 300  ## UPPER LIMIT OF NUMBER INSTANCES THAT WILL BE RETURNED, ADJUST ACCORDINGLY
 
 # Instead of training by epochs, detectron2 uses iterations. As far as I can tell, 1 iteration is a single instance.
 # Adjust this to determine how long the model trains.
 cfg.SOLVER.MAX_ITER = 20000   
-=======
-cfg.TEST.DETECTIONS_PER_IMAGE = 300
-
-# In[ ]:
->>>>>>> b4c32bc862a2b2b282635eb29ef6a4915a8a4921
 
 
 ##### Train with model checkpointing
 
-<<<<<<< HEAD
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True) # weights and tensorboard metrics will be stored here
 trainer = DefaultTrainer(cfg) 
 trainer.resume_or_load(resume=False) 
 
 train = False # make True to retrain, False to skip training (ie when you only want to evaluate)
-=======
-os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-trainer = DefaultTrainer(cfg) 
-trainer.resume_or_load(resume=False)
-train = False # make True to retrain
->>>>>>> b4c32bc862a2b2b282635eb29ef6a4915a8a4921
 if train:
     print('training model')
     trainer.train() # uncomment to retrain model
@@ -317,30 +234,14 @@ else:
     print('skipping training, using results from previous training')
 
 
-<<<<<<< HEAD
 # get path of model weight at each checkpoint, 
 # sorted in order of the number of training steps, followed by the last model
 checkpoint_paths = sorted(list(pathlib.Path(cfg.OUTPUT_DIR).glob('*model_*.pth')))
 
-=======
-
-# In[70]:
-
-
-##### 
-
-# get model checkpoints, sorted in order of the number of training steps, followed by the last model
-checkpoint_paths = sorted(list(pathlib.Path(cfg.OUTPUT_DIR).glob('*model_*.pth')))
-
-# # temp directory
-#checkpoint_paths = sorted(list(pathlib.Path('/media/ryan/TOSHIBA EXT/Research/Projects/AMPIS/output').glob('*model_*.pth')))
-
->>>>>>> b4c32bc862a2b2b282635eb29ef6a4915a8a4921
 ## TODO get losses at each checkpoint
 print('checkpoint paths found:\n\t{}'.format('\n\t'.join([x.name for x in checkpoint_paths])))
 
 
-<<<<<<< HEAD
 # Compute validation losses at each checkpoint
 # TODO finish this, it does not appear to work yet
 
@@ -361,24 +262,6 @@ data_val = dataval.build_detection_val_loader(cfg, None, ['powder_Validation'])
 # repeat for every checkpoint saved during training
 for p in checkpoint_paths:
 
-=======
-# In[71]:
-
-
-## visualize predictions of final model
-
-val_dir = os.path.join(str(cfg.OUTPUT_DIR)+'_validation')
-os.makedirs(val_dir, exist_ok=True)
-writers = ([
- CommonMetricPrinter(cfg.SOLVER.MAX_ITER),
-            JSONWriter(os.path.join(val_dir, "metrics_validation.json")),
-            TensorboardXWriter(val_dir),
-])
-
-data_val = dataval.build_detection_val_loader(cfg, None, ['powder_Validation'])
-
-for p in checkpoint_paths:
->>>>>>> b4c32bc862a2b2b282635eb29ef6a4915a8a4921
     cfg.MODEL.WEIGHTS = os.path.join(p)
     model = build_model(cfg)
     
@@ -393,17 +276,10 @@ for p in checkpoint_paths:
     )
 
     with EventStorage(start_iter) as storage:
-<<<<<<< HEAD
         for data in data_val: 
               
             storage.step()
             loss_dict = model(data) # compute validation losses
-=======
-        for data in data_val:
-              
-            storage.step()
-            loss_dict = model(data)
->>>>>>> b4c32bc862a2b2b282635eb29ef6a4915a8a4921
             losses = sum(loss_dict.values())
 
             assert torch.isfinite(losses).all(), loss_dict
@@ -413,45 +289,27 @@ for p in checkpoint_paths:
             if comm.is_main_process():
                 storage.put_scalars(total_loss=losses_reduced, **loss_dict_reduced)
             
-<<<<<<< HEAD
             for writer in writers: # write results to terminal, json, and tensorboard files
                 writer.write()
 
 
         ### visualization of predicted masks on all images
-=======
-            for writer in writers:
-                writer.write()
-
-
-        ### visualization of masks
->>>>>>> b4c32bc862a2b2b282635eb29ef6a4915a8a4921
         # make directory for output mask predictions
         outdir = '../figures/masks/predictions/{}'.format(p.stem)
         os.makedirs(outdir, exist_ok=True)
         predictor = DefaultPredictor(cfg)
         for dataset in ['powder_Training', 'powder_Validation']:
-<<<<<<< HEAD
             for d in DatasetCatalog.get(dataset): # TODO  replace with datasetloader to make this less hacky
-=======
-            for d in DatasetCatalog.get(dataset): # TODO  replace with datasetloader
->>>>>>> b4c32bc862a2b2b282635eb29ef6a4915a8a4921
                 img_path = pathlib.Path(d['file_name'])
                 print('image filename: {}'.format(img_path.name))
                 img = cv2.imread(str(img_path))
             
-<<<<<<< HEAD
             # overlay predicted masks on image
-=======
->>>>>>> b4c32bc862a2b2b282635eb29ef6a4915a8a4921
             out = predictor(img)
             v = Visualizer(img, metadata=MetadataCatalog.get(dataset))
             draw = v.draw_instance_predictions(out['instances'].to('cpu'))
             
-<<<<<<< HEAD
             # save images with standard formatting
-=======
->>>>>>> b4c32bc862a2b2b282635eb29ef6a4915a8a4921
             fig, ax = plt.subplots(figsize=(10,5), dpi=300)
             ax.imshow(draw.get_image())
             ax.set_title(title)
