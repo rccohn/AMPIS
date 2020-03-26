dcd95018a169b3f299c94b00b17139d625106f9e
Ryan Cohn <rcohn@cmu.edu>
2020-03-26 13:17:41 -0400

    finally finished spheroidite debugging

--- notebooks/explore_data_spheroidite.py
+++ notebooks/explore_data_spheroidite.py
@@ -1,10 +1,10 @@
 ##### Module imports
-
-gui = False
+gui = True
+import matplotlib
 if not gui:
     # make sure script doesn't break on non-gui jobs 
     # (ie batch job on computing cluster)
-    import matplotlib 
+    # for non gui, this needs to be set before pyplot or other libraries that use pyplot (ie seaborn, detectron visualizer, etc)
     matplotlib.use('agg')
 
 # regular module imports
@@ -16,10 +16,10 @@ import numpy as np
 import os
 import pathlib
 import pickle
-import re
 import skimage
 import skimage.io
 import skimage.measure
+from skimage.transform import resize as im_resize
 
 import mrcnn_utils
 
@@ -38,7 +38,7 @@ from detectron2.data.detection_utils import annotations_to_instances
 from detectron2.engine import DefaultTrainer, DefaultPredictor
 from detectron2.modeling import build_model
 from detectron2.solver import build_lr_scheduler, build_optimizer
-from detectron2.structures import BoxMode
+from detectron2.structures import Boxes, BoxMode
 import detectron2.utils.comm as comm
 from detectron2.utils.events import (
     CommonMetricPrinter,
@@ -48,8 +48,7 @@ from detectron2.utils.events import (
 )
 from detectron2.utils.visualizer import Visualizer
 
-#import dataval # custom module for getting loss stats # TODO figure out how to get more meaningful loss statistics
- 
+
 # verify cuda is installed and running correctly
 import torch
 from torch.utils.cpp_extension import CUDA_HOME
@@ -59,18 +58,27 @@ print(torch.cuda.is_available(), CUDA_HOME)
 # use subset of images without excessive amounts of instances for this experiment
 # filenames were randomly shuffled before saving
 #with open('spheroidite-files.pickle', 'rb') as f:
-with open('../data/raw/spheroidite/spheroidite-files.pickle', 'rb') as f:
+debug_desktop = False # sets directories
+if debug_desktop:
+    #data_root = pathlib.Path('/media/ryan/TOSHIBA EXT/Research/datasets/uhcs-segment/images/spheroidite/')
+    data_root = pathlib.Path('..','data','raw','spheroidite-images')
+else:
+    data_root = pathlib.Path('..', 'data', 'raw', 'spheroidite')
+
+assert data_root.is_dir()
+with open(pathlib.Path(data_root, 'spheroidite-files.pickle'), 'rb') as f:
     filename_subset = sorted(pickle.load(f))
 
+EXPERIMENT_NAME = 'spheroidite'  # TODO set up json control file with all parameters
 
-EXPERIMENT_NAME = 'spheroidite'    
-#data_root = pathlib.Path('/media/ryan/TOSHIBA EXT/Research/datasets/uhcs-segment/images/spheroidite/')
-data_root = pathlib.Path('..','data','raw','spheroidite')
-image_paths = {x.stem.replace('micrograph-','') : x for x in data_root.glob('micrograph*')}
-annotation_paths = {x.stem.replace('annotation-','') : x for x in data_root.glob('annotation*')}
+
+filename_selector = lambda pathname, pattern: pathname.stem.replace(pattern,'').split('_sizeRC')[0]
+image_paths = {filename_selector(x, 'micrograph-'): x for x in data_root.glob('micrograph*')}
+annotation_paths = {filename_selector(x, 'annotation-'): x for x in data_root.glob('annotation*')}
 
 image_subset = [image_paths.get(x) for x in filename_subset if image_paths.get(x) is not None]
 annotation_subset = [annotation_paths.get(x) for x in filename_subset if annotation_paths.get(x) is not None]
+
 
 img_train = image_subset[:-2]
 img_valid = image_subset[-2:]
@@ -78,13 +86,15 @@ img_valid = image_subset[-2:]
 ann_train = annotation_subset[:-2]
 ann_valid = annotation_subset[-2:]
 
-
 train_set = [img_train, ann_train, 'Training']
 valid_set = [img_valid, ann_valid, 'Validation']
+
+print('train_set: {}'.format([x.name for x in img_train]))
+print('valid set: {}'.format([x.name for x in img_valid]))
+
 datasets_all = {'Training': train_set, 
        'Validation': valid_set}
 
-    
 ##### Load data in a way that is consistent with detectron2 formatting
 
 def get_ddicts(img_paths, label_paths, dclass):
@@ -97,29 +107,21 @@ def get_ddicts(img_paths, label_paths, dclass):
     ddicts = []
     for ipath, lpath, d in zip(img_paths,label_paths, itertools.repeat(dclass)):
 
-        im = skimage.io.imread(lpath)
+        get_size = lambda path: [int(x) for x in path.stem.split('sizeRC_')[-1].split('_')]
+        imsize = get_size(ipath)
+        assert imsize == get_size(lpath)
 
-        r, c = im.shape
-
-        resized_im_path = pathlib.Path(ipath.parent, 'resized_'+ipath.name)
-        if not resized_im_path.is_file():
-            im_resize = skimage.io.imread(ipath, as_gray=True)[:r,:c]
-            skimage.io.imsave(resized_im_path, im_resize)
-
-
-	
-
-        ddict = {'file_name': str(resized_im_path),
+        ddict = {'file_name': str(ipath),
                  'annotation_file': str(lpath),
-                 'height': r,
-                 'width': c,
-                 'dataset_class':d,
+                 'height': imsize[0],
+                 'width': imsize[1],
+                 'dataset_class': d,
                  'mask_format': "bitmask"}
         
         ddicts.append(ddict)
-    
-    
+
     return ddicts
+
 
 def mapper(ddict):
     """
@@ -127,26 +129,21 @@ def mapper(ddict):
     input: ddict from list(dict) returned from get_ddicts() 
     returns: dataset_dict- data fully formatted for use with mask r-cnn
     """
-    ext_mapper = {'.tiff':255,
-                 '.png':0}
 
-    im_path = pathlib.Path(ddict['file_name'])
-    ann_path = pathlib.Path(ddict['annotation_file'])
-    im_b = skimage.io.imread(ddict['annotation_file']) == ext_mapper[ann_path.suffix]
+    img = cv2.imread(ddict['file_name']) # for whatever reason cv2 format works better here
+    ann = skimage.io.imread(ddict['annotation_file'], as_gray=True)
+    labels = skimage.measure.label(ann == 255)
 
-
-    img = skimage.io.imread(im_path, as_gray=True)
-    img = skimage.color.gray2rgb(img)
-    
-    labels = skimage.measure.label(im_b)
-    unique = np.unique(labels)[1:] # ignore 0 pixels 
+    unique = np.unique(labels)
+    if unique[0] == 0:
+        unique = unique[1:] # ignore background pixels labeled as 0
     
     n = unique.shape[0]
     
     annotations=[]
     for i in range(1,n+1):
         mask = labels == i
-        bbox = mrcnn_utils.extract_bboxes(mask[:,:,np.newaxis])[0]
+        bbox = mrcnn_utils.extract_bboxes(mask[:,:,np.newaxis])[0,[1,0,3,2]]
         label = 0
         annotations.append({'bbox': bbox,
                             'bbox_mode': BoxMode.XYXY_ABS,
@@ -161,45 +158,25 @@ def mapper(ddict):
     # Pytorch's dataloader is efficient on torch.Tensor due to shared-memory,
     # but not efficient on large generic data structures due to the use of pickle & mp.Queue.
     # Therefore it's important to use torch.Tensor.
-    dataset_dict["image"] = torch.as_tensor(np.ascontiguousarray(img.transpose(2, 0, 1)))
-    dataset_dict['annotations'] = annotations
+    dataset_dict["image"] = torch.as_tensor(img)
+    dataset_dict['annotations'] = instances
     
     
     return dataset_dict
 
 
-
 # Store the data so that detectron2 can work with it
 dataset_names = []
-for key, value in datasets_all.items():
+
+# USER: update thing_classes
+for key in datasets_all.keys():
     name = EXPERIMENT_NAME +'_'+key
-    DatasetCatalog.register(name, lambda: get_ddicts(*value))
+    DatasetCatalog.register(name, lambda k=key: get_ddicts(*datasets_all[k]))
     MetadataCatalog.get(name).set(thing_classes=["Spheroidite"])
     dataset_names.append(name)
 
-##### Verify ground-truth masks are loaded correctly
-# overlays ground truth instances on images and saves them. 
-
-for dataset in dataset_names:
-    for d in DatasetCatalog.get(dataset):
-        img_path = pathlib.Path(d['file_name'])
-        img = cv2.imread(str(img_path))
-        visualizer = Visualizer(img, metadata=MetadataCatalog.get(dataset), scale=1)
-        vis = visualizer.draw_dataset_dict(mapper(d))
-        fig, ax = plt.subplots(figsize=(10,5), dpi=300)
-        ax.imshow(vis.get_image())
-        ax.axis('off')
-        ax.set_title('{}\n{}'.format(dataset, img_path.name))
-        fig.tight_layout()
-        fig.savefig('../figures/masks/ground_truth/{}_{}.png'.format(dataset, img_path.stem))
-        if matplotlib.get_backend() is not 'agg': # if gui session is used, show images
-            plt.show()
-        plt.close('all')
-
-
 
 ##### Set up detectron2 configurations for Mask R-CNN model
-
 cfg = get_cfg()  # initialize configuration
 cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))  # use mask_rcnn with ResNet 50 backbone and feature pyramid network (FPN)
 
@@ -223,7 +200,7 @@ cfg.SOLVER.IMS_PER_BATCH = 1
 cfg.SOLVER.BASE_LR = 0.00025  # Learning rate (default is 0.00025)
 cfg.SOLVER.CHECKPOINT_PERIOD = 1000 
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512   # default: 512
-cfg.TEST.EVAL_PERIOD=cfg.SOLVER.CHECKPOINT_PERIOD
+cfg.TEST.EVAL_PERIOD = cfg.SOLVER.CHECKPOINT_PERIOD
 
 #  Number of different classes of instances that will be predicted.
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (powder particle)
@@ -237,12 +214,47 @@ cfg.TEST.DETECTIONS_PER_IMAGE = 600  ## UPPER LIMIT OF NUMBER INSTANCES THAT WIL
 cfg.SOLVER.MAX_ITER = 20000   
 
 
+##### Verify ground-truth masks are loaded correctly
+# overlays ground truth instances on images and saves them.
+
+if pathlib.Path(cfg.OUTPUT_DIR).is_dir():
+    import shutil
+    shutil.rmtree(cfg.OUTPUT_DIR)
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True) # weights and tensorboard metrics will be stored here
+
+outdir = pathlib.Path(str(cfg.OUTPUT_DIR))
+figure_root = pathlib.Path(outdir, 'Figures', 'masks')
+gt_figure_root = pathlib.Path(figure_root, 'ground_truth')
+pred_figure_root = pathlib.Path(figure_root, 'predicted')
+
+os.makedirs(gt_figure_root)
+os.mkdir(pred_figure_root)
+assert gt_figure_root.is_dir(), pred_figure_root.is_dir()
+for dataset in dataset_names:
+    for d in DatasetCatalog.get(dataset):
+        ddict_mapped = mapper(d)
+        img_path = pathlib.Path(ddict_mapped['file_name'])
+        img = ddict_mapped['image']
+        instances = ddict_mapped['annotations']
+        #print(dataset, d['file_name'])
+        visualizer = Visualizer(img, metadata=MetadataCatalog.get(dataset), scale=1)
+        vis = visualizer.overlay_instances(masks=instances.gt_masks, boxes=instances.gt_boxes)  # instances.gt_boxes, masks=None)#instances.gt_masks)
+        fig, ax = plt.subplots(figsize=(10,5), dpi=300)
+        ax.imshow(vis.get_image())
+        ax.axis('off')
+        ax.set_title('{}\n{}'.format(dataset, img_path.name))
+        fig.tight_layout()
+        fig_path = pathlib.Path(gt_figure_root, '{}_{}.png'.format(dataset, '{}'.format(img_path.stem)))
+        fig.savefig(fig_path, bbox_inches='tight')
+        if matplotlib.get_backend() is not 'agg':  # if gui session is used, show images
+            plt.show()
+        plt.close('all')
+
+
 ##### Train with model checkpointing
 
-os.makedirs(cfg.OUTPUT_DIR, exist_ok=True) # weights and tensorboard metrics will be stored here
-trainer = DefaultTrainer(cfg) 
-#trainer.build_hooks()
-trainer.resume_or_load(resume=False) 
+trainer = DefaultTrainer(cfg)
+trainer.resume_or_load(resume=False)
 
 train = True # make True to retrain, False to skip training (ie when you only want to evaluate)
 if train:
@@ -256,17 +268,9 @@ else:
 # sorted in order of the number of training steps, followed by the last model
 checkpoint_paths = sorted(list(pathlib.Path(cfg.OUTPUT_DIR).glob('*model_*.pth')))
 
-## TODO get losses at each checkpoint
 print('checkpoint paths found:\n\t{}'.format('\n\t'.join([x.name for x in checkpoint_paths])))
 
-
-# Compute validation losses at each checkpoint
-# TODO finish this, it does not appear to work yet
-
-# create a separate directory for validation loss outputs (json and tensorboard)
-val_dir = os.path.join(str(cfg.OUTPUT_DIR)+'_validation')
-os.makedirs(val_dir, exist_ok=True)
-
+os.makedirs(pathlib.Path(val_dir,"Figures","masks","ground_truth"))
 # set up writers
 writers = ([
  CommonMetricPrinter(cfg.SOLVER.MAX_ITER), # prints to terminal
@@ -274,42 +278,11 @@ writers = ([
             TensorboardXWriter(val_dir), # creates tensorboard log file with all constants
 ])
 
-# set up evaluator for validation set
-data_val = dataval.build_detection_val_loader(cfg, None, ['powder_Validation'])
-
 # repeat for every checkpoint saved during training
+
 for p in checkpoint_paths:
 
     cfg.MODEL.WEIGHTS = os.path.join(p)
-#     model = build_model(cfg)
-    
-#     optimizer = build_optimizer(cfg, model)
-#     scheduler = build_lr_scheduler(cfg, optimizer)
-
-#     checkpointer = DetectionCheckpointer(
-#     model, val_dir, optimizer=optimizer, scheduler=scheduler
-#     )
-#     start_iter = (
-#     checkpointer.resume_or_load(cfg.MODEL.WEIGHTS, resume=True).get("iteration", -1) + 1
-#     )
-
-#     with EventStorage(start_iter) as storage:
-#         for data in data_val: 
-              
-#             storage.step()
-#             loss_dict = model(data) # compute validation losses
-#             losses = sum(loss_dict.values())
-
-#             assert torch.isfinite(losses).all(), loss_dict
-
-#             loss_dict_reduced = {k: v.item() for k, v in comm.reduce_dict(loss_dict).items()}
-#             losses_reduced = sum(loss for loss in loss_dict_reduced.values())
-#             if comm.is_main_process():
-#                 storage.put_scalars(total_loss=losses_reduced, **loss_dict_reduced)
-            
-#             for writer in writers: # write results to terminal, json, and tensorboard files
-#                 writer.write()
-
 
         ### visualization of predicted masks on all images
         # make directory for output mask predictions
