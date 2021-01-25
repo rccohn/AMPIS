790b2c78bd6a6ebc4f2d63aaea9dd67607fa1fb9
Ryan Cohn <rcohn@andrew.cmu.edu>
2021-01-25 11:23:24 -0500

    fix typo, make build_environment work w cpu/cuda10

--- build_environment.sh
+++ build_environment.sh
@@ -5,12 +5,14 @@ python3.6 -m venv ampis_env
 
 source ampis_env/bin/activate
 
-pip install torch==1.4+cu100 torchvision==0.5+cu100 -f https://download.pytorch.org/whl/torch_stable.html
+CUDA='cpu' # tested with 'cu100' for cuda 10.0 and 'cpu' for cpu-only install
+
+pip install torch=="1.4+${CUDA}" torchvision=="0.5+${CUDA}" -f https://download.pytorch.org/whl/torch_stable.html
 
 pip install cython pyyaml==5.1
 
 pip install 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
 
-pip install scikit-image numy matplotlib opencv-python jupyterlab scikit-learn
+pip install scikit-image numpy matplotlib opencv-python jupyterlab scikit-learn
 
-pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu100/index.html
+pip install detectron2 -f "https://dl.fbaipublicfiles.com/detectron2/wheels/${CUDA}/index.html"
